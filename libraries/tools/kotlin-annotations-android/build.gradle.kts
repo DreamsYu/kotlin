12641fca2ea1e8f35c49e601412ad14d6f05e3fa
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2017-10-11 17:46:26 +0300

    fixup! Rename android-annotations -> kotlin-annotations-android

--- libraries/tools/kotlin-annotations-android/build.gradle.kts
+++ libraries/tools/kotlin-annotations-android/build.gradle.kts
@@ -4,13 +4,13 @@ description = "Kotlin annotations for Android"
 
 apply { plugin("kotlin") }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jdkHome = rootProject.extra["JDK_16"]!!.toString()
-    kotlinOptions.jvmTarget = "1.6"
+jvmTarget = "1.6"
+javaHome = rootProject.extra["JDK_16"] as String
 
+tasks.withType<KotlinCompile> {
     kotlinOptions.freeCompilerArgs += listOf(
             "-Xallow-kotlin-package",
-            "-module-name", "kotlin.annotations.android"
+            "-module-name", project.name
     )
 }
 
